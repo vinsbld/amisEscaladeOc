e95942e4d76e010e38094c8865be51e0e6901eac
B65473 <B65473@LAPTOP-572BDHV9>
2019-10-30 11:56:52 +0100

    0_FristCommit

---
+++ amisEscalade/src/main/java/com/oc/AmisEscaladeApplication.java
@@ -0,0 +1,13 @@
+package com.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmisEscaladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmisEscaladeApplication.class, args);
+	}
+
+}
