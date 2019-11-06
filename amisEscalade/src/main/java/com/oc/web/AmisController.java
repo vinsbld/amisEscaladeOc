75256b6ae870463039b39705365e3158a3c53faa
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-06 14:50:20 +0100

    addControler

---
+++ amisEscalade/src/main/java/com/oc/web/AmisController.java
@@ -0,0 +1,20 @@
+package com.oc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AmisController {
+	
+	@GetMapping("/inscription")
+	public String inscription() {
+		return "formInscription";
+		
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+}
