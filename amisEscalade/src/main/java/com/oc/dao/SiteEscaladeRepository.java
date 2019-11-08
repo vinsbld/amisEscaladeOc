d2987965b94a5a182e9679ff3304ad0adb7f7352
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-08 10:52:32 +0100

    addSiteEscaladeDAO

---
+++ amisEscalade/src/main/java/com/oc/dao/SiteEscaladeRepository.java
@@ -0,0 +1,13 @@
+package com.oc.dao;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oc.entities.SiteEscalade;
+
+@Repository
+public interface SiteEscaladeRepository extends JpaRepository<SiteEscalade, Long>{
+
+}
