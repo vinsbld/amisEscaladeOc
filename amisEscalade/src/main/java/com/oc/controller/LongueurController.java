d87626cd2552744b3f285925049d05d90a499ada
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-11 12:52:03 +0100

    addControllerLongueur

--- amisEscalade/src/main/java/com/oc/controller/LongueurController.java
+++ amisEscalade/src/main/java/com/oc/controller/LongueurController.java
@@ -1,5 +1,6 @@
 package com.oc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.oc.forms.LongueurForm;
-import com.oc.forms.SecteurForm;
+import com.oc.metier.LongueurService;
+
 
 @Controller
 @RequestMapping("/formLongeur")
@@ -20,6 +22,9 @@ public class LongueurController {
 	public String formLong() {
 		return "formLongeur";
 	}
+	
+	@Autowired
+	private LongueurService longueurService;
 	
 	@PostMapping
 	public String ajouterLongueur(Model model, @ModelAttribute("longueurForm") LongueurForm longueurForm,
