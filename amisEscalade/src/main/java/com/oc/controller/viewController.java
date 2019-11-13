b87404dff877809417bd64d1ec64984ab3c3bffd
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-13 10:34:28 +0100

    addViewController&StyleCss&createFrontPage

---
+++ amisEscalade/src/main/java/com/oc/controller/viewController.java
@@ -0,0 +1,20 @@
+package com.oc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class viewController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/association")
+	public String asso() {
+		return "association";
+	}
+
+}
