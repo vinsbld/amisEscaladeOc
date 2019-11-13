42ce19a3cd1d675de1534167e5b881858990c3a8
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-13 11:00:42 +0100

    updateSecurityConfig

--- amisEscalade/src/main/java/com/oc/security/SecurityConfig.java
+++ amisEscalade/src/main/java/com/oc/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import com.oc.metier.UserGrimpService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
-	private UserGrimpService userGrimpService;
+	private UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService((UserDetailsService) userGrimpService);
+        auth.setUserDetailsService(userDetailsService);
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
