6d371d29042dfc382f924e7a284128f0bc05a2bd
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-20 15:03:17 +0100

    addQueryParamForSearchBarSector

--- amisEscalade/src/main/java/com/oc/dao/SecteurRepository.java
+++ amisEscalade/src/main/java/com/oc/dao/SecteurRepository.java
@@ -1,6 +1,10 @@
 package com.oc.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.oc.entities.Secteur;
@@ -8,5 +12,6 @@ import com.oc.entities.Secteur;
 
 @Repository
 public interface SecteurRepository extends JpaRepository<Secteur, Long> {
-
+@Query("select sec from Secteur sec where sec.nomDuSecteur like:x")
+public Page<Secteur> chercher(@Param("x")String motCle, Pageable pageable);
 }
