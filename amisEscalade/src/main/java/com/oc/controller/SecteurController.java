5ce2f64e91e4e2d530650eeca5fd7ec4877d723a
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-21 16:31:54 +0100

    newFileHtml&createUpdateDeleteOnController&view

--- amisEscalade/src/main/java/com/oc/controller/SecteurController.java
+++ amisEscalade/src/main/java/com/oc/controller/SecteurController.java
@@ -10,12 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.oc.dao.SecteurRepository;
+import com.oc.dao.SiteEscaladeRepository;
 import com.oc.entities.Secteur;
 import com.oc.entities.SiteEscalade;
 import com.oc.forms.SecteurForm;
@@ -43,25 +45,53 @@ public class SecteurController {
 			return "formSecteur";
 		}
 		
-		secteurService.saveSecteurForm(secteurForm, result);
+		secteurService.saveSecteur(secteurForm, result);
 		
-		return "addSecteurToSiteEscalade";
+		return "secteur";
 	}
 		
 	
-	  @GetMapping("/addSecteurToSiteEscalade") public String secteurSiteEscal(Model model,
-	  
-	  @RequestParam(name="page", defaultValue="0")int p,
-	  
-	  @RequestParam(name="size", defaultValue="4")int s,
-	  
+	  @GetMapping("/secteur") public String secteurSiteEscal(Model model,
+	  @RequestParam(name="page", defaultValue="0")int p,	  
+	  @RequestParam(name="size", defaultValue="4")int s,	  
 	  @RequestParam(name="motCle", defaultValue="")String motCle) { Page<Secteur>
 	  pageSecteurs = secteurRepository.chercher("%"+motCle+"%", new PageRequest(p,s));
 	  
 	  model.addAttribute("listSecteur", pageSecteurs.getContent()); int[]pages =
 	  new int[pageSecteurs.getTotalPages()]; model.addAttribute("pages", pages);
 	  model.addAttribute("size", s); model.addAttribute("pageCourante", p);
-	  model.addAttribute("motCle", motCle); return "addSecteurToSiteEscalade"; }
+	  model.addAttribute("motCle", motCle); 
+	  return "secteur"; 
+	  }
 	 
-	
+	  @GetMapping("/secteur/{idSecteur}/edit")
+	  public String editSecteur(@PathVariable("idSecteur") long idSecteur, Model model) {
+		  Optional<Secteur> sec = secteurRepository.findById(idSecteur);
+		  
+		  Secteur addSecteur = null;
+		  
+		  if(sec.isPresent()) {
+				addSecteur= sec.get();
+			}
+			model.addAttribute("addSecteur", addSecteur);
+			
+			return"editFormSecteur";
+		}
+	  
+	  @PostMapping("/secteur/{idSecteur}/update")
+	  public String updateSecteur(@PathVariable("idSecteur") long idSecteur, Model model, @ModelAttribute("editFormSecteur") SecteurForm secteurForm, BindingResult result, final RedirectAttributes redirectAttributes){
+		secteurService.saveSecteur(secteurForm, result);  
+		return"redirect:/secteur";
+	  }
+	  
+	  @GetMapping("/secteur/{idSecteur}/delete")
+	  public String deleteSecteur(@PathVariable("idSecteur") long idSecteur, Model model, final RedirectAttributes redirectAttributes) {
+		  
+		  secteurRepository.deleteById(idSecteur);
+		  
+		  return "redirect:/secteur";
+	  }
+	  
+		  
 }
+	  
