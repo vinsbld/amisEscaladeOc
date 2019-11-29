79406d29bb0df919104dc2c32be386a3719a8d73
B65473 <B65473@LAPTOP-572BDHV9>
2019-11-29 18:52:00 +0100

    cleanQuery

--- amisEscalade/src/main/java/com/oc/dao/SiteEscaladeRepository.java
+++ amisEscalade/src/main/java/com/oc/dao/SiteEscaladeRepository.java
@@ -2,8 +2,7 @@ package com.oc.dao;
 
 
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +12,5 @@ import com.oc.entities.SiteEscalade;
 
 @Repository
 public interface SiteEscaladeRepository extends JpaRepository<SiteEscalade, Long>{	
-	  @Query("select site from SiteEscalade site where site.nomSiteEscalade like:x")
-	 public Page<SiteEscalade> chercher(@Param("x")String motCle, Pageable pageable);
-	
 	
 }
