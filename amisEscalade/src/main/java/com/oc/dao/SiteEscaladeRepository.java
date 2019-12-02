99f749861c6a200fb0b4dafdcea4f87dff572106
B65473 <B65473@LAPTOP-572BDHV9>
2019-12-02 14:59:03 +0100

    cleanSiteEscaladeRepo

--- amisEscalade/src/main/java/com/oc/dao/SiteEscaladeRepository.java
+++ amisEscalade/src/main/java/com/oc/dao/SiteEscaladeRepository.java
@@ -4,10 +4,7 @@ package com.oc.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.oc.entities.SiteEscalade;
 
 @Repository
