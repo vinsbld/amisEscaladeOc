0995022cee8c247eb1c8a5bcf9a5e2dad68bc32d
B65473 <B65473@LAPTOP-572BDHV9>
2020-01-14 11:57:41 +0100

    commentsDateFormat&alignBtnUserPage

--- amisEscalade/src/main/java/com/oc/web/ReservationController.java
+++ amisEscalade/src/main/java/com/oc/web/ReservationController.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
