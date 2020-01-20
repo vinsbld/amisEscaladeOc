d72ab96d1b77d0ec852b49fe1455f2f46ca73e1a
B65473 <B65473@LAPTOP-572BDHV9>
2020-01-20 14:33:34 +0100

    editErrorsMessagesWorks

--- amisEscalade/src/main/java/com/oc/web/UserGrimpController.java
+++ amisEscalade/src/main/java/com/oc/web/UserGrimpController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,16 +72,20 @@ public class UserGrimpController {
 			result.rejectValue("username", "user.pseudo", "ce pseudo est déjà utilisé :(");
 			model.addAttribute("userGrimpForm", userGrimpForm);
 			return "formInscription";
+			
 		}else if(userGrimpRepository.getUsrEmail(userGrimpForm.getEmail()) !=null ) {
+			
 			result.rejectValue("email", "user.email", "cet e-mail est déjà associé à un compte utilisateur :(");
 			model.addAttribute("userGrimpForm", userGrimpForm);
 			return "formInscription";
+			
 		}else if(userGrimpForm.getUsername().length() < 2 && userGrimpForm.getUsername().length() > 30 ||
 				userGrimpForm.getPassword().length() < 4){
+			
 			model.addAttribute("userGrimpForm", userGrimpForm);	
 			return "formInscription";
-		}
-		else {
+			
+		}else {
 		UserGrimp newUserGrimp = new UserGrimp();
 		newUserGrimp.setPseudo(userGrimpForm.getUsername().toLowerCase());
 		newUserGrimp.setEmail(userGrimpForm.getEmail().toLowerCase());
@@ -93,7 +98,7 @@ public class UserGrimpController {
 	
 	/*============== #Modification ======================*/
 	@GetMapping("/profil/edit")
-	public String editProfil(Model model) {
+	public String editProfil(Model model, @ModelAttribute("userForm") UserGrimpForm userGrimpf) {
 		
 		UserGrimp usr = (UserGrimp) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		UserGrimpForm userForm = new UserGrimpForm();
@@ -106,33 +111,35 @@ public class UserGrimpController {
 	}
 	
 	@PostMapping("/profil/update")
-	public String updateProfil(Model model, @ModelAttribute("userGrimp") UserGrimpForm userGrimp, BindingResult result,
+	public String updateProfil(Model model, @ModelAttribute("userForm") UserGrimpForm userGrimpf, BindingResult result,
 			final RedirectAttributes redirectAttributes) {
 		
 		UserGrimp usr = (UserGrimp) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		UserGrimp ur = userGrimpRepository.findByPseudo(userGrimp.getUsername());
+		UserGrimp ur = userGrimpRepository.findByPseudo(userGrimpf.getUsername().toLowerCase());
 				
 		if (result.hasErrors()) {
-			model.addAttribute("userForm", userGrimp);
+			model.addAttribute("userForm", userGrimpf);
 			return "editFormInscription";
 			
 		}else if (ur !=null && ur.getIdUserGrimp() != usr.getIdUserGrimp()) {
-			result.rejectValue("username", "user.pseudo", "ce pseudo est déjà utilisé :(");
-			model.addAttribute("userForm", userGrimp);
+			
+			
+			result.rejectValue("username", "user.name", "ce pseudo est déjà utilisé :(");
+			model.addAttribute("userForm", userGrimpf);
 			
 			return "editFormInscription";
 			
 		}else if (ur.getEmail() != null && usr.getIdUserGrimp() != ur.getIdUserGrimp()) {
 			
 			result.rejectValue("email", "user.email", "cet e-mail est déjà associé à un compte utilisateur :(");
-			model.addAttribute("userForm", userGrimp);
+			model.addAttribute("userForm", userGrimpf);
 			
 			return "editFormInscription";
 		}		
 		else {
-		usr.setPseudo(userGrimp.getUsername().toLowerCase());
-		usr.setEmail(userGrimp.getEmail().toLowerCase());
-		usr.setPassword(userGrimp.getPassword());
+		usr.setPseudo(userGrimpf.getUsername().toLowerCase());
+		usr.setEmail(userGrimpf.getEmail().toLowerCase());
+		usr.setPassword(userGrimpf.getPassword());
 		userGrimpRepository.save(usr);
 		}
 		return "redirect:/profil";
